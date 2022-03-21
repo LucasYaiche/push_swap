fb0b4895f85e7561704c2c8380411753c0a72c80
Lucas Yaiche <lyaiche@fu-r10-p1.s19.be>
2022-03-21 19:29:09 +0100

    seems good

--- common/check_argv.c
+++ common/check_argv.c
@@ -6,11 +6,28 @@
 /*   By: lyaiche <lyaiche@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 15:46:10 by lyaiche           #+#    #+#             */
-/*   Updated: 2022/03/21 16:54:39 by lyaiche          ###   ########.fr       */
+/*   Updated: 2022/03/21 19:28:01 by lyaiche          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	check_argv_2(char **argv, int i)
+{
+	if (argv[i][0] != '-')
+	{
+		if ((ft_strlen(argv[i]) >= 10)
+			&& (ft_strncmp("2147483647", argv[i], ft_strlen(argv[i])) < 0))
+			return (0);
+	}
+	else if (argv[i][0] == '-')
+	{	
+		if (ft_strlen(argv[i]) >= 11
+			&& ft_strncmp("-2147483648", argv[i], 11) < 0)
+			return (0);
+	}
+	return (1);
+}
 
 int	check_argv(int argc, char **argv)
 {
@@ -31,16 +48,8 @@ int	check_argv(int argc, char **argv)
 			if (ft_isdigit(argv[i][j]) && (argv[i][j + 1] == '-'))
 				return (0);
 		}
-		if (argv[i][0] != '-')
-		{
-			if ((ft_strlen(argv[i]) >= 10)
-				&& (ft_strncmp("2147483647", argv[i], ft_strlen(argv[i])) < 0))
-				return (0);
-		}
-		else if (argv[i][0] == '-')
-			if (ft_strlen(argv[i]) >= 11
-				&& ft_strncmp("-2147483648", argv[i], 11) < 0)
-				return (0);
+		if (!check_argv_2(argv, i))
+			return (0);
 	}
 	return (1);
 }
